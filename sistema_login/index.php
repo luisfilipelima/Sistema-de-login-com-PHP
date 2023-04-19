8473af4f01903162f85beab7fb30eeb3c2112ed2
Luís Filipe <110272982+luisfilipelima@users.noreply.github.com>
2023-04-19 20:25:26 -0300

    criando sistema de login

---
+++ sistema_login/index.php
@@ -0,0 +1,31 @@
+ <!DOCTYPE html>
+ <html lang="pt-br">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <title>Sistema de loign</title>
+ </head>
+ <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 offset-lg-4">
+                <form action="login.php" method="POST">
+                    <div class="mb-3">
+                             <label for="">Usuário</label>
+                          <input type="text" name="usuario" class="f">
+                    </div>
+                    <div class="mb-3">
+                          
+                    </div>
+                    <div class="mb-3">
+                          
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+ </body>
+ </html>
